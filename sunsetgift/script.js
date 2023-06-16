e55e977b1e31cc07fafcc996b49dd7b516fdea17
bagasjn <bagasjulio241@gmail.com>
2023-06-16 16:36:49 +0700

    first commit

--- sunsetgift/script.js
+++
@@ -1,8 +0,0 @@
-// toggle kelas aktive
-const navbarNav = document.querySelector('.navbar-nav');
-
-//ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').oneclick = () => {
-  navbarNav.classList.toggle('active');
-};
-
